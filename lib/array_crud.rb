865d9a8cf4b8fdfe42e077ffa8cf1941fe51399f
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-04 06:32:24 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -43,5 +43,5 @@ end
 
 def update_element_from_index(array, index_number, element)
 array = ["wow", "I", "am", "really", "learning", "arrays!"]
-array[index_number]
+array[index_number]=element
 end
