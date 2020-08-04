5e2ff30211361696e1d10044f2f87d9676edf630
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-04 06:07:19 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -4,12 +4,12 @@ def create_an_empty_array
 end
 
 def create_an_array
-  four_elements=[1,2,3,5]
+  four_elements=[1,2,3,4]
 end
 
 def add_element_to_end_of_array(array, element)
   add_element=[1,2,3]
-  add_element<< "arrays!"
+  add_element<< 4
 end
 
 def add_element_to_start_of_array(array, element)
