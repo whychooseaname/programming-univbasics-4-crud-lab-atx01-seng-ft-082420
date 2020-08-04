71db1255cef032fc1fec7318ed6bb65b5d20704b
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-04 06:17:35 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -13,7 +13,7 @@ def add_element_to_end_of_array(array, element)
 end
 
 def add_element_to_start_of_array(array, element)
-  add_element_to_start_of_array[]
+  add_element_to_start_of_array=[]
   add_element_to_start_of_array.unshift[1]
 end
 
