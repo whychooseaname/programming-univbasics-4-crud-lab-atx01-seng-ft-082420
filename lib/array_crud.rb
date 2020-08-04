b2875746284cdc3123b312381b7c40d067a3f0a9
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-04 06:19:22 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -18,7 +18,8 @@ def add_element_to_start_of_array(array, element)
 end
 
 def remove_element_from_end_of_array(array)
-  
+  array=[1,2]
+  array.pop
 end
 
 def remove_element_from_start_of_array(array)
