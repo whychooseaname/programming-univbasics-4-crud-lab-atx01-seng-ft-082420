39a40f0493b2239585365fed1263e5d155b9c287
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-04 06:14:27 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -8,8 +8,7 @@ def create_an_array
 end
 
 def add_element_to_end_of_array(array, element)
-  add_element=[1,2,3]
-  add_element<< "arrays!"
+  add_element<< "add"
 end
 
 def add_element_to_start_of_array(array, element)
