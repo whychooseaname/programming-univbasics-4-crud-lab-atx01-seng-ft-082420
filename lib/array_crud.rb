84ba3a74c68d6b538fa13f11a32c213b116ab546
Edward Acosta <edward.ea.acosta@gmail.com>
2020-08-04 06:24:47 +0000

    Automatically backed up by Learn

--- lib/array_crud.rb
+++ lib/array_crud.rb
@@ -23,7 +23,7 @@ end
 
 def remove_element_from_start_of_array(array)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  
+  array.unshift
 end
 
 def retrieve_element_from_index(array, index_number)
